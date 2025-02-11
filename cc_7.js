18fd03ac0ab9cbcd4a2edf675b44f55e06d78ac7
Riley Hedges <rhedges1@usf.edu>
2025-02-11 18:26:59 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -13,3 +13,17 @@ function calculateInvoice(subtotal, taxRate, discount) {
 calculateInvoice(100, .08, 5);
 calculateInvoice(500, .1, 20);
 
+/////////////////////////////////
+// Task 2: Function Expression //
+/////////////////////////////////
+
+console.log("--------------------------------------");
+console.log("Task 2: Function Expression");
+
+let calculateHourlyWage = function(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek * 52);
+    console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`)
+}
+
+calculateHourlyWage(52000, 40);
+calculateHourlyWage(75000, 35);
